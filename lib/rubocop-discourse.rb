5f088579d6512607ad84714108555b62e3e5fe8c
Jarek Radosz <jradosz@gmail.com>
2025-02-20 15:04:34 +0100

    DEV: Migrate to rubocop 1.72 plugin system
    See: https://docs.rubocop.org/rubocop/plugin_migration_guide.html
    
    Also adds syntax_tree and bumps gh actions.

--- lib/rubocop-discourse.rb
+++ lib/rubocop-discourse.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
 require "rubocop"
+require "lint_roller"
 require "active_support"
 require "active_support/core_ext/string/inflections"
 require "active_support/core_ext/object/blank"
 require "active_support/core_ext/enumerable"
-require_relative "rubocop/discourse"
-require_relative "rubocop/discourse/inject"
-
-RuboCop::Discourse::Inject.defaults!
-
 require_relative "rubocop/cop/discourse_cops"
+require_relative "rubocop/discourse/version"
+require_relative "rubocop/discourse/plugin"
